4c906502ddfdd6b0b480c75c56d2bd25ba2d5233
Enzzzzz <contatoenzz@gmail.com>
2024-11-13 15:21:47 -0300

    added languages for two commands

---
+++ Language/setLanguageLang.js
@@ -0,0 +1,13 @@
+const LanguageLang = {
+    en: {
+        languageSelectedTitle: "Language Selected",
+        languageSelectedDescription: "I will now operate in English on your server. Feel free to change it whenever you like!",
+    },
+    pt: {
+        languageSelectedTitle: "Idioma Selecionado",
+        languageSelectedDescription: "Agora vou operar em Português no seu servidor. Fique à vontade para mudar a qualquer momento!",
+    },
+};
+  
+  
+  module.exports = { LanguageLang };
---
+++ Language/setWelcomeLang.js
@@ -0,0 +1,20 @@
+const WelcomeLang = {
+    en: {
+        cmdDesc: "⚒️ Set a channel for welcome messages",
+        success: "Welcome channel set to",
+        noChannel: "You didn't provide a valid channel.",
+        notFound: "No settings found for this server.",
+        invalidChannel: "The channel you provided is invalid.",
+    },
+    pt: {
+        cmdDesc: "⚒️ Defina um canal para mensagens de boas-vindas",
+        success: "Canal de boas-vindas definido para",
+        noChannel: "Você não forneceu um canal válido.",
+        notFound: "Nenhuma configuração encontrada para este servidor.",
+        invalidChannel: "O canal fornecido é inválido.",
+    },
+  };
+  
+  
+  module.exports = { WelcomeLang };
+  
