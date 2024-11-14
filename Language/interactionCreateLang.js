46182b16d288409aac30c2696b24a4a7ef620234
Enzzzzz <contatoenzz@gmail.com>
2024-11-14 12:53:09 -0300

    added chinese language

--- Language/interactionCreateLang.js
+++ Language/interactionCreateLang.js
@@ -1,13 +1,16 @@
 const InteractionCreateLang = {
-    en: {
-      welcomeTitle: "Welcome to the server",
-      welcomeDescription: "We hope you enjoy our server.",
-    },
-    pt: {
-      welcomeTitle: "Bem-vindo ao servidor,",
-      welcomeDescription: "Esperamos que você aproveite o nosso servidor.",
-    },
-  };
+  en: {
+    welcomeTitle: "Welcome to the server",
+    welcomeDescription: "We hope you enjoy our server.",
+  },
+  pt: {
+    welcomeTitle: "Bem-vindo ao servidor,",
+    welcomeDescription: "Esperamos que você aproveite o nosso servidor.",
+  },
+  cn: {
+    welcomeTitle: "欢迎来到服务器",
+    welcomeDescription: "希望你喜欢我们的服务器。",
+  }
+};
 
-  module.exports = { InteractionCreateLang };
-  
+module.exports = { InteractionCreateLang };
